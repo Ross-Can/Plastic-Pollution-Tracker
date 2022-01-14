6c082f14b58b5406df9f850eea132e6cee2e3927
Matthew Ross <31318280+Ross-Can@users.noreply.github.com>
2022-01-13 22:11:14 -0500

    [fixed] table loading; changed level indicator
    Level Indicator changed to hover rather than click. Changes mouse icon instead of placing pins

--- version 0.1/script.js
+++ version 0.1/script.js
@@ -1,65 +1,13 @@
-function goToWebPage(webPage) {
-  //changes the website location
-  window.location = webPage;
-}
+$.get(
+  "https://services6.arcgis.com/C0HVLQJI37vYnazu/arcgis/rest/services/Estimate_of_Plastic_Pollution_in_the_World_s_Oceans_1_01_4_75/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json",
+  function (items) {
+    items.features.forEach((loc) => {
+      filter(loc.attributes);
+    });
 
-$("#mapButton").click(function () {
-  goToWebPage("map.html");
-});
-
-var ocean;
-//function navigateTo(location){
-//var loc = location.replace(" ", "+")
-//var google_loc = "https://www.google.com/maps/dir/?api=1&destination=" + loc + "&travelmode=walking"
-//goToWebPage(google_loc)
-//}
-
-var image = document.createElement("img");
-
-$("area").click(function () {
-  ocean = event.target;
-  displayInfo(ocean.title);
-});
-
-function displayInfo(oceanClicked) {
-  if (oceanClicked == "Atlantic Ocean") writeToTable(Atlantic);
-  else if (oceanClicked == "Pacific Ocean") writeToTable(Pacific);
-  else if (oceanClicked == "Indian Ocean") writeToTable(Indian);
-  else if (oceanClicked == "Southern Ocean") writeToTable(Southern);
-  else if (oceanClicked == "Arctic Ocean") writeToTable(Arctic);
-  else if (oceanClicked == "Mediterranean Sea") writeToTable(Maditerranean);
-  else if (oceanClicked == "Baltic Sea") writeToTable(Baltic);
-}
-
-function placeImage(left, top, level) {
-  image.src = "Image/level" + level + ".png";
-  image.style.position = "absolute";
-  image.style.zIndex = "3";
-
-  if (level == 1) changeSize(130, 70);
-  if (level == 2) changeSize(150, 150);
-  if (level == 3) changeSize(150, 150);
-
-  image.style.left = left - image.width / 2 + "px";
-  image.style.top = top - image.height / 2 + "px";
-
-  document.getElementById("mainContent").appendChild(image);
-  console.log("click");
-}
-
-function changeSize(height, width) {
-  image.style.width = width + "px";
-  image.style.height = height + "px";
-}
-
-function changeContinent(continent_name, amount_found) {
-  document.getElementById("continent").innerHTML = continent_name;
-  document.getElementById("found").innerHTML =
-    amount_found + " Contaminents Found";
-}
-
-var locationItems = [];
-var average;
+    average = 268;
+  }
+);
 
 let filter = (loc) => {
   if (Atlantic.contains(loc)) Atlantic.add(loc);
@@ -71,42 +19,63 @@ let filter = (loc) => {
   else if (Mediterranian.contains(loc)) Mediterranian.add(loc);
 };
 
-$.get(
-  "https://services6.arcgis.com/C0HVLQJI37vYnazu/arcgis/rest/services/Estimate_of_Plastic_Pollution_in_the_World_s_Oceans_1_01_4_75/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json",
-  function (items) {
-    locationItems = items.features;
+$("area").hover(function () {
+  displayOceanData(this.title);
+});
 
-    locationItems.forEach((loc) => {
-      filter(loc.attributes);
-    });
+let displayOceanData = (oceanClicked) => {
+  if (oceanClicked == "Atlantic Ocean") displayData(Atlantic);
+  else if (oceanClicked == "Pacific Ocean") displayData(Pacific);
+  else if (oceanClicked == "Indian Ocean") displayData(Indian);
+  else if (oceanClicked == "Southern Ocean") displayData(Southern);
+  else if (oceanClicked == "Arctic Ocean") displayData(Arctic);
+  else if (oceanClicked == "Mediterranean Sea") displayData(Mediterranean);
+  else if (oceanClicked == "Baltic Sea") displayData(Baltic);
+};
 
-    console.log("pacific amount: ", Pacific.opMaxLong());
-    average = 653;
-  }
-);
+let displayData = (Ocean) => {
+  changeTableTitle(Ocean);
+  changeCursor(Ocean);
+  writeToTable(Ocean);
+};
 
-function writeToTable(Ocean) {
-  console.log(Ocean);
-  changeContinent(Ocean.name(), Ocean.coordinateCount());
-  if (Ocean.count > average - average / 3)
-    placeImage(event.clientX, event.clientY, 3);
-  else if (Ocean.count > average - (average / 3) * 2)
-    placeImage(event.clientX, event.clientY, 2);
-  else placeImage(event.clientX, event.clientY, 1);
-  var tableRow = document.getElementById("tableRow");
-  tableRow.innerHTML = "";
-  for (let i = 0; i < Ocean.count; i++) {
-    tableRow.innerHTML +=
-      "<tr>" +
-      "<th scope='row'>" +
-      (i + 1) +
-      "</th>" +
-      "<td>" +
-      array[i].LONGITUDE +
-      "</td>" +
-      "<td>" +
-      array[i].LATITUDE +
-      "</td>" +
-      "</tr>";
-  }
+let changeTableTitle = (Ocean) => {
+  document.getElementById("ocean").innerHTML = Ocean.name();
+  document.getElementById("found").innerHTML =
+    Ocean.coordinateCount() + " Instances Found";
+};
+
+let writeToTable = (Ocean) => {
+  let table = document.getElementById("tableRow");
+  table.innerHTML = Ocean.getTable();
+};
+
+function hideTable() {}
+
+let changeCursor = (Ocean) => {
+  let count = Ocean.coordinateCount();
+  if (count > average - average / 3) cursorLevel(3);
+  else if (count > average - (average / 3) * 2) cursorLevel(2);
+  else cursorLevel(1);
+};
+
+let cursorLevel = (level) => {
+  $("area").css({
+    cursor: "url('assets/cursors/level" + level + "-cursor.png'), auto",
+  });
+};
+
+function goToWebPage(webPage) {
+  //changes the website location
+  window.location = webPage;
 }
+
+$("#mapButton").click(function () {
+  goToWebPage("map.html");
+});
+
+//function navigateTo(location){
+//var loc = location.replace(" ", "+")
+//var google_loc = "https://www.google.com/maps/dir/?api=1&destination=" + loc + "&travelmode=walking"
+//goToWebPage(google_loc)
+//}
